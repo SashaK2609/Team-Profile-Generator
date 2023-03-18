78a5a1dd36428a88cf0724e35b030e43a257e3b8
SashaKorobkina <s.dekaiper@gmail.com>
2023-03-18 00:01:41 +0000

    defined and exported the Employee class

--- starter/lib/Employee.js
+++ starter/lib/Employee.js
@@ -1,1 +1,31 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+	id;
+	name;
+	email;
+	role = 'Employee';
+
+	constructor(id, name, email) {
+		 this.id = id;
+		 this.name = name;
+		 this.email = email;
+	}
+
+	getRole() {
+		 return this.role;
+	}
+
+	getId() {
+		 return this.id;
+	}
+	getName() {
+		 return this.name;
+	}
+	getEmail() {
+		 return this.email;
+	}
+}
+
+module.exports = Employee;
+
